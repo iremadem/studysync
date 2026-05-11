6b5e0f23960930955998006c211249385896c76e
sdilaraertn <sudedilaraertan@gmail.com>
2026-05-11 03:57:46 +0300

    Fix ALLOWED_HOSTS

--- studysync/settings.py
+++ studysync/settings.py
@@ -29,7 +29,7 @@ ANTHROPIC_API_KEY = os.environ.get('ANTHROPIC_API_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'studysync-mzb1.onrender.com']
 
 
 # Application definition
